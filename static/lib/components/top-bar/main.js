1df66268a562f9a174242996e189dfb2b601af0f
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-03-18 22:03:16 +0100

    add subtitle attribute

--- static/lib/components/top-bar/main.js
+++ static/lib/components/top-bar/main.js
@@ -2,12 +2,16 @@ export class Component {
   constructor(options) {
     this.document = options.shadowDom;
     this.title = this.document.getElementById("title");
+    this.subtitle = this.document.getElementById("subtitle");
   }
 
   attributeChangedCallback(attribute, oldValue, newValue) {
     switch (attribute) {
       case "title":
         this.title.innerText = newValue;
+        break;
+      case "subtitle":
+        this.subtitle.innerText = newValue;
         break;
     }
   }
