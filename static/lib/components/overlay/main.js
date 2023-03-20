28d2577313855016a7fda4c99a0976290017dc4b
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-03-20 21:50:50 +0100

    ok I admit some of this code is a little bit fucked up

--- static/lib/components/overlay/main.js
+++ static/lib/components/overlay/main.js
@@ -1,5 +1,21 @@
 export class Component {
   constructor(options) {
     this.document = options.shadowDom;
+    this.overlay = this.document.getElementById("overlay");
+    this.arrow = this.document.getElementById("arrow");
+
+    this.overlay.addEventListener("click", (e) => {
+      if (
+        this.overlay.dataset.focused == "true" &&
+        e.composedPath().includes(this.arrow)
+      )
+        this.overlay.dataset.focused = "false";
+      else this.overlay.dataset.focused = "true";
+    });
+
+    document.addEventListener("click", (e) => {
+      if (!e.composedPath().includes(this.overlay))
+        this.overlay.dataset.focused = "false";
+    });
   }
 }
